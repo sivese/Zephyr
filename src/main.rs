e34b4ee4cbae76fe964281b6e273d73b39cc9d37
Doss Lee <driversivese@gmail.com>
2025-10-08 23:23:05 -0700

    first, simple, api test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Router, Json,
+    extract::{multipart, Multipart}, http::StatusCode, response::Json, routing::{get, post}, Router
 };
+
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app = Router::new()
+        .route("/", post(handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn handler(mut multipart: Multipart) -> Json<serde_json::Value> {
+    let response = json!({
+        "message": "Hello, World!"
+    });
+
+    Json(response)
 }
